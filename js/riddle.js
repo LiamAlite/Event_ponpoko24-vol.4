aa78844d0181670dc5083b2d054bc175018e948b
Lyla <k.kanno@jointpocket.com>
2020-04-30 18:21:29 +0900

    hidden riddle

--- js/riddle.js
+++ js/riddle.js
@@ -2,10 +2,8 @@ var num = 0;
 $("#omesis").click(function(){
     $(this).data("click", ++num);
     var click = $(this).data("click");
-    console.log(click)
     if(click == 5){
         $("#riddle").modal("show");
-        
         click = 0
         num = 0
     }
