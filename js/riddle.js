62b9da1bdca59c7039838a73fb661f32acd7f9db
Lyla <k.kanno@jointpocket.com>
2020-04-30 18:09:28 +0900

    riddle test

---
+++ js/riddle.js
@@ -0,0 +1,13 @@
+var num = 0;
+$("#omesis").click(function(){
+    $(this).data("click", ++num);
+    var click = $(this).data("click");
+    console.log(click)
+    if(click == 5){
+        $("#riddle").modal("show");
+        
+        click = 0
+        num = 0
+    }
+    return false;
+});
