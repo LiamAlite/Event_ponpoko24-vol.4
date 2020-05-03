d3b8208655183c1270e4ec11ff59eb759858d2b0
Lyla <k.kanno@jointpocket.com>
2020-05-04 06:17:24 +0900

    add secret guest, exclude pokoart.html

---
+++ js/random.js
@@ -0,0 +1,21 @@
+(function($){
+    $(function() {
+
+        var rBox = [];
+        $('.elements').each(function() {
+            rBox.push($(this).html());
+        });
+        rBox.sort(function() {
+            return Math.random() - Math.random();
+        });
+        $('.elements').empty();
+
+        i = 0;
+        $('.elements').each(function() {
+            $(this).append(rBox[i]);
+            i++;
+        });
+
+    });
+})(jQuery);
+//ランダムに入れ替えたい要素にclass="elements"を付与
